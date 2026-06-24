46630adbd04a270c73a2d813d09cc9f88dac77dd
Ganesh Ramchandra <aiexploreradventurerwonder@gmail.com>
2026-06-24 13:45:09 +0530

    Add pinokio.js with module exports and menu

---
+++ pinokio.js
@@ -0,0 +1,16 @@
+module.exports = {
+  version: "2.0",
+  title: "IOPaint",
+  description: "AI-powered image inpainting. Remove objects, people, defects from images.",
+  icon: "icon.png",
+  menu: [
+    {
+      text: "Install",
+      href: "install.json",
+    },
+    {
+      text: "Start",
+      href: "start.json",
+    }
+  ]
+}
